5188a60e6ddd57b4cd69b0f8748ce7fea6f0c40c
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2023-01-16 14:51:22 +0000

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -10,7 +10,7 @@ function About() {
             <div className="pb-10 mb-10">
                 <HeadText color="black" text="About Me"/>
                 <div className="text-center">
-                    <AboutMeCard/>
+                    <AboutMeCard/> 
                     <div className="mt-11">
                         <Link href="https://drive.google.com/file/d/1zAWpsFyVYj3HlCnqdCqYLh8A4_26a_HD/view?usp=sharing"><a className="bg-[#1C9841] duration-700 hover:bg-[#083f26] pt-2 text-white pb-2 pl-24 pr-24 rounded-full">My Resume</a></Link>
                         <SocialLinks/>
